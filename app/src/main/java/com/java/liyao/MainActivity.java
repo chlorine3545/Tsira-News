c8d528e86ac949f16b131c4c59abfa1a76beb4b2
Chlorine <chlorine-dev@tuta.io>
2024-07-21 22:11:40 +0800

    修复了主活动异常导致的实机闪退问题

--- app/src/main/java/com/java/liyao/MainActivity.java
+++ app/src/main/java/com/java/liyao/MainActivity.java
@@ -59,143 +59,151 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-        if (networkInfo == null || !networkInfo.isConnected()) {
-            Toast.makeText(this, "网络连接失败，请检查网络连接！", Toast.LENGTH_SHORT).show();
-        }
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            if (networkInfo == null || !networkInfo.isConnected()) {
+                Toast.makeText(this, "网络连接失败，请检查网络连接！", Toast.LENGTH_SHORT).show();
+            }
 
-        // CatPrefDbHelper.getInstance(this).resetAllUserPreferences(allCats);
-        // HistoryDbHelper.getInstance(this).deleteAllHistory();
+            // CatPrefDbHelper.getInstance(this).resetAllUserPreferences(allCats);
+            // HistoryDbHelper.getInstance(this).deleteAllHistory();
 
-        if (userInfo != null) {
-            cats = CatPrefDbHelper.getInstance(this).getCatPrefList(userInfo.getUser_email());
-        }
-        else {
-            cats = CatPrefDbHelper.getInstance(this).getCatPrefList("null"); // 未登录
-            // 先用 "null" 作为未登录的邮箱，可以避免直接使用 null 带来的亿点啸问题
-            // Log.d("未登录的偏好情况", "onCreate: " + cats.toString());
-        }
+            if (userInfo != null) {
+                cats = CatPrefDbHelper.getInstance(this).getCatPrefList(userInfo.getUser_email());
+            }
+            else {
+                cats = CatPrefDbHelper.getInstance(this).getCatPrefList("null"); // 未登录
+                // 先用 "null" 作为未登录的邮箱，可以避免直接使用 null 带来的亿点啸问题
+                // Log.d("未登录的偏好情况", "onCreate: " + cats.toString());
+            }
 
-        // 初始化控件
-        viewPg2 = findViewById(R.id.viewPg2);
-        catTab = findViewById(R.id.catTab);
-        nav_view = findViewById(R.id.nav_view);
-        main_toolbar = findViewById(R.id.main_toolbar);
-        // 主打一个严谨
-        search = main_toolbar.findViewById(R.id.search_layout).findViewById(R.id.search_box);
-        View headerView = nav_view.getHeaderView(0);
-        tv_nickname = headerView.findViewById(R.id.tv_nickname);
-        tv_user_email = headerView.findViewById(R.id.tv_user_email);
-        search_icon = main_toolbar.findViewById(R.id.search_icon);
+            // 初始化控件
+            viewPg2 = findViewById(R.id.viewPg2);
+            catTab = findViewById(R.id.catTab);
+            nav_view = findViewById(R.id.nav_view);
+            main_toolbar = findViewById(R.id.main_toolbar);
+            // 主打一个严谨
+            search = main_toolbar.findViewById(R.id.search_layout).findViewById(R.id.search_box);
+            View headerView = nav_view.getHeaderView(0);
+            tv_nickname = headerView.findViewById(R.id.tv_nickname);
+            tv_user_email = headerView.findViewById(R.id.tv_user_email);
+            search_icon = main_toolbar.findViewById(R.id.search_icon);
 
-        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                if (item.getItemId() == R.id.nav_history) {
-                    Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
-                    startActivity(intent);
-                } else if (item.getItemId() == R.id.nav_like) {
-                    Intent intent = new Intent(MainActivity.this, LikeActivity.class);
-                    startActivity(intent);
-                } else if (item.getItemId() == R.id.nav_category) {
-                    Intent intent = new Intent(MainActivity.this, EditCategoriesActivity.class);
-                    startActivity(intent);
-                } else if (item.getItemId() == R.id.nav_logout) {
-                    userInfo.logOut();
-                    Toast.makeText(MainActivity.this, "已退出登录！", Toast.LENGTH_SHORT).show();
-                    onResume();
-                } else if (item.getItemId() == R.id.nav_account) {
-                    Intent intent;
-                    if (userInfo == null) {
-                        intent = new Intent(MainActivity.this, LoginActivity.class);
-                    } else {
-                        intent = new Intent(MainActivity.this, AccountActivity.class);
+                    if (item.getItemId() == R.id.nav_history) {
+                        Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+                        startActivity(intent);
+                    } else if (item.getItemId() == R.id.nav_like) {
+                        Intent intent = new Intent(MainActivity.this, LikeActivity.class);
+                        startActivity(intent);
+                    } else if (item.getItemId() == R.id.nav_category) {
+                        Intent intent = new Intent(MainActivity.this, EditCategoriesActivity.class);
+                        startActivity(intent);
+                    } else if (item.getItemId() == R.id.nav_logout) {
+                        userInfo.logOut();
+                        Toast.makeText(MainActivity.this, "已退出登录！", Toast.LENGTH_SHORT).show();
+                        onResume();
+                    } else if (item.getItemId() == R.id.nav_account) {
+                        Intent intent;
+                        if (userInfo == null) {
+                            intent = new Intent(MainActivity.this, LoginActivity.class);
+                        } else {
+                            intent = new Intent(MainActivity.this, AccountActivity.class);
+                        }
+                        startActivity(intent);
+                    } else if (item.getItemId() == R.id.nav_about) {
+                        Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                        startActivity(intent);
                     }
-                    startActivity(intent);
-                } else if (item.getItemId() == R.id.nav_about) {
-                    Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                    // 剩下的一会再写
+                    return true;
+                }
+            });
+
+            main_toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+                    drawerLayout.open();
+                }
+            });
+
+            // 设置adapter
+            viewPg2.setAdapter(new FragmentStateAdapter(this) {
+                @NonNull
+                @Override
+                public Fragment createFragment(int position) {
+                    //创建 NewsTabFragment页面
+                    String title = cats.get(position);
+                    CatTabFragment ctf = CatTabFragment.newInstance(title);
+                    return ctf;
+                }
+
+                @Override
+                public int getItemCount() {
+                    if (cats == null) {
+                        cats = new ArrayList<>(allCats);
+                    }
+                    return cats.size();
+                }
+            });
+
+            // tab_layout点击事件
+            catTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+                @Override
+                public void onTabSelected(TabLayout.Tab tab) {
+                    // 设置viewPager选中当前页
+                    viewPg2.setCurrentItem(tab.getPosition(), true);
+                }
+
+                @Override
+                public void onTabUnselected(TabLayout.Tab tab) {
+
+                }
+
+                @Override
+                public void onTabReselected(TabLayout.Tab tab) {
+
+                }
+            });
+
+            // viewPager和tab_layout关联在一起
+            TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(catTab, viewPg2, new TabLayoutMediator.TabConfigurationStrategy() {
+                @Override
+                public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                    tab.setText(cats.get(position));
+                }
+            });
+
+            tabLayoutMediator.attach();
+
+            // 搜索框输入事件。要不还是不做搜索框，直接点击跳转到搜索页面吧。
+            search.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, SearchActivity.class);
                     startActivity(intent);
                 }
-                // 剩下的一会再写
-                return true;
-            }
-        });
+            });
 
-        main_toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
-                drawerLayout.open();
-            }
-        });
+            // 搞个怪
+            search_icon.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "前面的区域，以后再来探索吧！", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (Exception e) {
+            Log.e("TSIRA", "onCreate: 启动失败", e);
+        }
 
-        // 设置adapter
-        viewPg2.setAdapter(new FragmentStateAdapter(this) {
-            @NonNull
-            @Override
-            public Fragment createFragment(int position) {
-                //创建 NewsTabFragment页面
-                String title = cats.get(position);
-                CatTabFragment ctf = CatTabFragment.newInstance(title);
-                return ctf;
-            }
-
-            @Override
-            public int getItemCount() {
-                return cats.size();
-            }
-        });
-
-        // tab_layout点击事件
-        catTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                // 设置viewPager选中当前页
-                viewPg2.setCurrentItem(tab.getPosition(), true);
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-        // viewPager和tab_layout关联在一起
-        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(catTab, viewPg2, new TabLayoutMediator.TabConfigurationStrategy() {
-            @Override
-            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-                tab.setText(cats.get(position));
-            }
-        });
-
-        tabLayoutMediator.attach();
-
-        // 搜索框输入事件。要不还是不做搜索框，直接点击跳转到搜索页面吧。
-        search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
-                startActivity(intent);
-            }
-        });
-        
-        // 搞个怪
-        search_icon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "前面的区域，以后再来探索吧！", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
     @Override
